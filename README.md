c1bcb5cfb9830b727cae9c36d8f6a42a098f6f91
whitemordred <joss@dev2real.com>
2023-08-02 11:39:00 +0000

    Add Csv extension

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 
-# JSON Normalization for Dataset Creation
+# JSON and CSV Normalization for Dataset Creation
 
-This Python script is designed to normalize JSON files for the purpose of creating datasets. It reads JSON files from a source directory, normalizes them according to a specified configuration file, and saves the normalized files to a destination directory.
+This Python script is designed to normalize JSON and CSV files for the purpose of creating datasets. It reads files from a source directory, normalizes them according to a specified configuration file, and saves the normalized files to a destination directory. The script can handle both JSON and CSV file formats for both input and output.
 
 ## Usage
 
-To use this script, ensure you have the source and destination folders as well as the configuration file in the same directory as the script.
-
-The script is to be run with Python and requires no command line options.
+To use this script, ensure you have the source and destination folders as well as the configuration file in the same directory as the script. The file formats for input and output are specified via command line arguments.
 
 ```bash
-python normalization_tool.py
+python normalization_tool.py --ext_src json --ext_dest csv
 ```
+
+In this example, the script will convert JSON files in the source directory to CSV files in the destination directory.
 
 ## File Structure
 
-The script assumes that each source file is a list of JSON objects. Each object in the list is normalized according to the configuration file.
+The script assumes that each source file is a list of JSON objects or a CSV file. Each object or row is normalized according to the configuration file.
 
 The configuration file is a JSON file that defines the structure of the normalized object. Each key in the configuration file corresponds to a key in the normalized object, and the associated value indicates the corresponding key in the source object.
 
@@ -44,6 +44,8 @@ The script requires Python and the following libraries:
 - os
 - uuid
 - datetime
+- pandas
+- argparse
 
 ## License
 
--- normalization_tool.py
+++ normalization_tool.py
@@ -1,22 +1,28 @@
-import json
 import os
 import uuid
 from datetime import datetime
+import pandas as pd
+import json
+import argparse
 
-def normalize(src_dir, dest_dir, config_path):
+def normalize(src_dir, dest_dir, config_path, input_extension, output_extension):
     # Load the config file
     with open(config_path, 'r') as f:
         config = json.load(f)
 
+    # Define the input and output methods based on the extensions
+    load_method = pd.read_json if input_extension == 'json' else pd.read_csv
+    save_method = pd.DataFrame.to_json if output_extension == 'json' else pd.DataFrame.to_csv
+
     # Iterate through all the files in the source directory
     for filename in os.listdir(src_dir):
-        if filename.endswith('.json'):
+        if filename.endswith('.' + input_extension):
             # Load the source file
-            with open(os.path.join(src_dir, filename), 'r') as f:
-                data_list = json.load(f)
+            df = load_method(os.path.join(src_dir, filename))
 
             new_data_list = []
-            for data in data_list:
+            for _, row in df.iterrows():
+                data = row.to_dict()
                 # Create a new dictionary based on the config
                 new_data = {}
                 for key, value in config.items():
@@ -31,16 +37,25 @@ def normalize(src_dir, dest_dir, config_path):
 
                 new_data_list.append(new_data)
 
-            # Save the new data to the destination directory
-            with open(os.path.join(dest_dir,"normalized_"+filename), 'w') as f:
-                json.dump(new_data_list, f, ensure_ascii=False, indent=4)
+            # Convert the list of dictionaries to a DataFrame
+            df = pd.DataFrame(new_data_list)
 
-# Get the current script directory
-current_dir = os.path.dirname(os.path.realpath(__file__))
+            # Save the DataFrame to a file in the destination directory
+            save_method(df, os.path.join(dest_dir, filename.replace('.' + input_extension, '.' + output_extension)), index=False)
 
-# Define the source, destination, and config paths
-src_dir = os.path.join(current_dir, 'src')
-dest_dir = os.path.join(current_dir, 'dest')
-config_path = os.path.join(current_dir, 'config.json')
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Normalize datasets.')
+    parser.add_argument('--ext_src', type=str, help='Source file extension (json or csv)', required=True)
+    parser.add_argument('--ext_dest', type=str, help='Destination file extension (json or csv)', required=True)
+    args = parser.parse_args()
 
-normalize(src_dir, dest_dir, config_path)
+    # Get the current script directory
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Define the source, destination, and config paths
+    src_dir = os.path.join(current_dir, 'src')
+    dest_dir = os.path.join(current_dir, 'dest')
+    config_path = os.path.join(current_dir, 'config.json')
+
+    normalize(src_dir, dest_dir, config_path, args.ext_src, args.ext_dest)
